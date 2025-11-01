acddc3c16e16deb67aa98c8a2afbb12fbbd8f9ab
IseAkira <akira.ise.m@gmail.com>
2025-11-01 22:41:40 +0900

    iwrite

--- section4-react/src/components/pages/Login.js
+++ section4-react/src/components/pages/Login.js
@@ -1,6 +1,36 @@
-import { Container, Box, Typography } from '@mui/material';
+import { Container, Box, Typography, TextField, Button } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const onClickLogin = () => {
+    console.log('ログイン処理実行');
+    const endpoint = 'https://jsonplaceholder.typicode.com/users';
+    const queries = { username: user.username, id: user.password };
+    axios.get(endpoint, { params: queries }).then((res) => {
+      console.log(res.data[0]);
+      if (res.data[0] === undefined) {
+        console.log('ユーザーが存在しません');
+        navigate('/loginfailed');
+      } else {
+        console.log('ログイン成功');
+        navigate('/');
+      }
+    });
+  };
+
   return (
     <>
       <Container maxwidth="xs">
@@ -13,6 +43,39 @@ const Login = () => {
           }}
         />
         <Typography variant="h5">ログイン画面</Typography>
+        <TextField
+          margin="normal"
+          required
+          placeholder="・・・"
+          id="username"
+          autoComplete="current-password"
+          name="username"
+          label="名前"
+          fullWidth
+          onChange={handleChange}
+        />
+        <TextField
+          margin="normal"
+          required
+          placeholder="••••••"
+          id="password"
+          autoComplete="current-password"
+          name="username"
+          label="パスワード"
+          fullWidth
+          onChange={handleChange}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          onClick={onClickLogin}
+        >
+          ログイン
+        </Button>
+
+        <Link to="/register">新規登録はこちら</Link>
       </Container>
     </>
   );

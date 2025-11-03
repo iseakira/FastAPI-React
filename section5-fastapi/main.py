4e20df8705010752876a56b6aa7f743eabd653f4
IseAkira <akira.ise.m@gmail.com>
2025-11-04 01:51:09 +0900

    軽微な修正

--- section5-fastapi/main.py
+++ section5-fastapi/main.py
@@ -5,6 +5,7 @@ app = FastAPI()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+#パスパラメーター
+@app.get("/items/{item_id}")
+def reat_item(item_id: int):
+    return {"item_id": item_id}

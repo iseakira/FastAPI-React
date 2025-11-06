d138d64ddc2e71488f21e5b833700c3cfdcfd133
IseAkira <akira.ise.m@gmail.com>
2025-11-07 02:13:08 +0900

    dababaseの構築

---
+++ section7-fastapi/backend/crud.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+def get_users(db:Session):
+  return db.query(models.User).all()
+
+def get_user(db:Session,user_id:int):
+  return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_name(db:Session,username:str):
+  return db.query(models.User).filter(models.User.name == username).first()
+
+def get_user_by_name_bypassword(db:Session,username:str,password:str):
+  return db.query(models.User).filter(models.User.name == username).filter(models.User.password == password).first()
+
+
+

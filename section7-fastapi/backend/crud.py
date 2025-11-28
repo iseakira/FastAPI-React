7c30b51f0dade2c455841fff8d875daf5fdbadd2
IseAkira <akira.ise.m@gmail.com>
2025-11-28 13:41:31 +0900

    userの慣習増やし

--- section7-fastapi/backend/crud.py
+++ section7-fastapi/backend/crud.py
@@ -14,5 +14,12 @@ def get_user_by_name(db:Session,username:str):
 def get_user_by_name_bypassword(db:Session,username:str,password:str):
   return db.query(models.User).filter(models.User.name == username).filter(models.User.password == password).first()
 
+def create_user(db:Session,user:schemas.UserCreate):
+  db_user = models.User(name=user.username,password=user.password)
+  db.add(db_user)
+  db.commit()
+  db.refresh(db_user)
+  return db_user
+
 
 

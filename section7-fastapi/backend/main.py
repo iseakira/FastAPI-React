91a6cd70509841bece42465b85de7c65439af711
IseAkira <akira.ise.m@gmail.com>
2025-12-02 21:27:00 +0900

    read_userAPIの作成

--- section7-fastapi/backend/main.py
+++ section7-fastapi/backend/main.py
@@ -21,3 +21,10 @@ def create_user(user:schemas.UserCreate, db:Session=Depends(get_db)):
   if db_user:
     raise HTTPException(status_code=400, detail="Username already registered")
   return crud.create_user(db=db,user=user)
+
+@app.get("/users/",response_model=schemas.User)
+def read_user(name:str,password:str, db:Session=Depends(get_db)):
+  db_user = crud.get_user_by_name_bypassword(db,username=name,password=password)
+  if db_user is None:
+    raise HTTPException(status_code=404, detail="User not found")
+  return db_user

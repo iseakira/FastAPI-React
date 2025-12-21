020e3a9b3883586907d54d3453c47745ef06a124
IseAkira <akira.ise.m@gmail.com>
2025-12-22 01:27:01 +0900

    おわり

---
+++ section4-react/src/components/elements/BasicMenu.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useNavigate } from 'react-router-dom';
+
+export default function PositionedMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const navigate = useNavigate();
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClickHome = () => {
+    navigate('/');
+  };
+  const handleClickLogin = () => {
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      <Button
+        id="demo-positioned-button"
+        aria-controls={open ? 'demo-positioned-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        color="inherit"
+      >
+        MENUE
+      </Button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={handleClickHome}>ホーム</MenuItem>
+        <MenuItem onClick={handleClickLogin}>ログアウト</MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- section4-react/src/components/templates/Header.jsx
+++ section4-react/src/components/templates/Header.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import BasicMenu from '../elements/BasicMenu';
+import { Grid } from '@mui/material';
 
 const Header = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static">
+      <Toolbar>
+        <Grid container>
+          <Grid size={6}>
+            <BasicMenu />
+          </Grid>
+          <Grid size={6} sx={{ textAlign: 'center' }}>
+            <Typography variant="p" component="div">
+              ログインユーザー:
+            </Typography>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 };
 

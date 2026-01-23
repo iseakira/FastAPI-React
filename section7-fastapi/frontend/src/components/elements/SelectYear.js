2351124d0651f11685ed8248aab8b1015334a9ed
IseAkira <akira.ise.m@gmail.com>
2026-01-23 19:12:34 +0900

    selectyear開発

---
+++ section7-fastapi/frontend/src/components/elements/SelectYear.js
@@ -0,0 +1,28 @@
+import { Box, InputLabel, MenuItem, Select } from '@mui/material';
+import React from 'react';
+
+const SelectYear = (props) => {
+  const { handleYearChange } = props;
+  const handleChange = (e) => {
+    handleYearChange(e.target.value);
+  };
+  return (
+    <>
+      <Box sx={{ width: '100%' }}>
+        <InputLabel id="sales-year">年度</InputLabel>
+        <Select
+          labelId="sales-year"
+          onChange={handleChange}
+          fullWidth
+          name="year"
+        >
+          <MenuItem value={2020}>2020</MenuItem>
+          <MenuItem value={2021}>2021</MenuItem>
+          <MenuItem value={2022}>2022</MenuItem>
+        </Select>
+      </Box>
+    </>
+  );
+};
+
+export default SelectYear;
--- section7-fastapi/frontend/src/components/pages/Home.js
+++ section7-fastapi/frontend/src/components/pages/Home.js
@@ -5,12 +5,17 @@ import { LoginUserContext } from '../providers/LoginUserProvider';
 import { useContext } from 'react';
 import ReadCsv from '../elements/ReadCsv';
 import Barchart from '../elements/BarChart';
+import SelectYear from '../elements/SelectYear';
 
 const Home = () => {
   const { LoginUser, isLogined } = useContext(LoginUserContext);
   const [data, setData] = useState([]);
   const handleDataChange = (newData) => {
     setData(newData);
+  };
+  const [year, setYear] = useState();
+  const handleYearChange = (newYear) => {
+    setYear(newYear);
   };
 
   if (!isLogined) {
@@ -21,6 +26,7 @@ const Home = () => {
         <Headrer />
         <h3>CSVをデータベースで格納する</h3>
         <ReadCsv handleDataChange={handleDataChange} />
+        <SelectYear handleYearChange={handleYearChange} />
         ホーム画面:{LoginUser}
         <h3>グラフ表示</h3>
         <Barchart data={data} />

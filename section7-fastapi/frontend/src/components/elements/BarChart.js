6f24e3f2c57cc8ce2b86a8bb8dcaf2413475f6ee
IseAkira <akira.ise.m@gmail.com>
2026-01-23 18:49:08 +0900

    barchartの開発

--- section7-fastapi/frontend/src/components/elements/BarChart.js
+++ section7-fastapi/frontend/src/components/elements/BarChart.js
@@ -9,52 +9,60 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-);
-
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-};
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-const chartdata = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
+import { Grid } from '@mui/material';
 
 const BarChart = (props) => {
   const { data } = props;
   console.log(data);
-  return <Bar options={options} data={chartdata} />;
+  if (data === undefined || data.length === 0) {
+    return <>データはなし</>;
+  }
+  const obj = {};
+  for (let i = 0; i < data.length; i++) {
+    obj[data[i][0]] = data[i].slice(1);
+  }
+  console.log(obj);
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  );
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+    },
+  };
+
+  const labels = obj[Object.keys(obj)[0]];
+  const chartdata = {
+    labels,
+    datasets: [
+      {
+        label: obj['year'][0],
+        data: obj['sales'],
+        backgroundColor: 'rgba(99, 172, 255, 0.5)',
+      },
+    ],
+  };
+  return (
+    <Grid container justifyContent="center">
+      <Grid item xs={12} md={8}>
+        <div style={{ width: '600px', height: '300px' }}>
+          部署ごとの売上
+          <Bar options={options} data={chartdata} />
+        </div>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default BarChart;
